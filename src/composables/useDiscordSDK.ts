d7ca4906d725d8a286e45701d31d288410c385ed
SuhEugene <suheugene@ya.ru>
2024-04-24 02:49:34 +0300

    Add DiscordSDK mock

--- src/composables/useDiscordSDK.ts
+++ src/composables/useDiscordSDK.ts
@@ -1,12 +1,20 @@
-import { DiscordSDK } from '@discord/embedded-app-sdk';
+import { DiscordSDK, DiscordSDKMock } from '@discord/embedded-app-sdk';
 import { onMounted, readonly, ref } from 'vue';
 
-let discordSdk: DiscordSDK | null = null;
+let discordSdk: DiscordSDK | DiscordSDKMock | null = null;
 const ready = ref(false);
 
 export default function useDiscordSDK() {
+  const params = new URLSearchParams(window.location.search);
+
   if (discordSdk === null)
-    discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+    discordSdk = params.get('frameId')
+      ? new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID)
+      : new DiscordSDKMock(
+        import.meta.env.VITE_DISCORD_CLIENT_ID,
+        "946712507874152488",
+        "1191473895086239824"
+      );
 
   onMounted(async () => {
     if (ready.value || !discordSdk) return;
